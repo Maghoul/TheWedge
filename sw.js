c5a56b413f547f2dfe3069674b7fbc92ab3fd649
Maghoul <griffinrw@hotmail.com>
2025-06-01 16:57:52 -0600

    small index update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wedge-cache-v1.1.9';
+const CACHE_NAME = 'wedge-cache-v1.1.10';
 // Cache versioning to handle updates vMajor.minor.patch
 // Version 1.0.1 - Initial release with basic caching functionality
 // Version 1.0.2 - updated styles for ios support
@@ -13,6 +13,7 @@ const CACHE_NAME = 'wedge-cache-v1.1.9';
 // Version 1.1.7 - Improved TAF readability in wx.js
 // Version 1.1.8 - Dept/Arr error handling and improved styling for results
 // Version 1.1.9 - Added alternate logic for wx.js
+// Version 1.1.10 - small index update
 // This service worker caches essential files for offline use and handles fetch requests
 const urlsToCache = [
     './',
